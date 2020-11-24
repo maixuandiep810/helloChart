bdde1adcabac32b015ed1300f98e95776068d5c3
maixuandiep810 <maixuandiep810@gmail.com>
2020-11-24 16:49:05 +0700

    add lineChart

--- helloChart/LineChartController.swift
+++ helloChart/LineChartController.swift
@@ -8,15 +8,17 @@
 import UIKit
 import Charts
 
-class LineChartController: UIViewController {
+class LineChartController: UIViewController, ChartViewDelegate {
     
-    @IBOutlet weak var mainChart: LineChartView!
+    @IBOutlet weak var mainChart: UIView!
     @IBOutlet weak var dataTF: UITextView!
+    var lineChartView = LineChartView()
     
     var numbers : [Double] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        lineChartView.delegate = self
         // Do any additional setup after loading the view.
         
     }
@@ -29,16 +31,21 @@ class LineChartController: UIViewController {
     
     
     @IBAction func addDataTouchUpIS(_ sender: Any) {
-                let num = Double(dataTF.text!)!
-        dataTF.text = ""
+        let value = dataTF.text == "" ? "1" : dataTF.text
+                let num = Double(value!)!
+                dataTF.text = ""
                 numbers.append(num)
+        updateGraph()
     }
-    //    @IBAction func addDataTouchUpIS(_ sender: Any) {
-//        let num = Double(dataTF.text!)!
-//        numbers.append(num)
-//    }
     
     func updateGraph(){
+        
+        lineChartView.frame = CGRect(x: mainChart.frame.origin.x, y: mainChart.frame.origin.y, width: mainChart.frame.size.width, height: mainChart.frame.size.height)
+        lineChartView.center = mainChart.center
+        
+        view.addSubview(lineChartView)
+        numbers = [Double](arrayLiteral: 1, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2, 2, 2.3, 2.4, 2.3, 2.2, 2, 1.9, 2.2)
+        
         var lineChartEntry  = [ChartDataEntry]() //this is the Array that will eventually be displayed on the graph.
         
         
@@ -56,8 +63,8 @@ class LineChartController: UIViewController {
         data.addDataSet(line1) //Adds the line to the dataSet
         
 
-        mainChart.data = data //finally - it adds the chart data to the chart and causes an update
-        mainChart.chartDescription?.text = "My awesome chart" // Here we set the description for the graph
+        lineChartView.data = data //finally - it adds the chart data to the chart and causes an update
+        lineChartView.chartDescription?.text = "My awesome chart" // Here we set the description for the graph
     }
 
 
